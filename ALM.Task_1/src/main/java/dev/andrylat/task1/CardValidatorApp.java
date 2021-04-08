c060d1ee9eec5f2782bafc975f8da0c9f035892b
Yudin_DA <Denum@mail.ru>
2021-04-08 08:31:23 +0300

    initial commit

---
+++ ALM.Task_1/src/main/java/dev/andrylat/task1/CardValidatorApp.java
@@ -0,0 +1,13 @@
+package dev.andrylat.task1;
+
+import dev.andrylat.task1.service.UserInterface;
+
+public class CardValidatorApp {
+    
+    public static void main(String[] args) {
+        
+        
+    }
+}
+
+
